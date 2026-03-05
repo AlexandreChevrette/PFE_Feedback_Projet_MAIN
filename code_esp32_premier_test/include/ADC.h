966f4dbc21d0e3289c5233f27f5a1a1fa4d550f4
AlexandreChevrette <alche67@ulaval.ca>
2026-03-05 14:58:44 -0500

    update ADC code

--- code_esp32_premier_test/include/ADC.h
+++ code_esp32_premier_test/include/ADC.h
@@ -3,6 +3,45 @@
 
 #include <Arduino.h>
 #include "Variables.h"
+#include <stdint.h>
+#include <array>
+
+const int numberOfChannels = 3;
+
+class ADC{
+    public:
+        ADC();
+        void reset();
+        void setGain(); // set to one for this code
+        void setSampleRate(int p_sampleSpeed);
+        void setupChannel(int p_channel); // enable and differential
+        void disableChannel(int p_channel);
+        void readValues(std::array<float, numberOfChannels>& p_values);
+        
+    
+    private:
+        std::array<float, numberOfChannels> m_adcValues;
+        uint32_t spiTransfer24(uint32_t p_data);
+        void writeRegister(uint8_t p_reg, uint16_t p_value); 
+        uint16_t readRegister(uint8_t p_reg);
+        void ADC::readData(int32_t* p_channels);
+};
+
+
+
+/*chat me dit de mettre ça? 
+volatile bool dataReady = false;
+
+void IRAM_ATTR drdyISR()
+{
+    dataReady = true;
+}
+
+void setup()
+{
+    pinMode(14, INPUT);
+    attachInterrupt(14, drdyISR, FALLING);
+}*/
 
 
 #endif 
--- code_esp32_premier_test/src/ADC.cpp
+++ code_esp32_premier_test/src/ADC.cpp
@@ -1,2 +1,88 @@
 #include <Arduino.h>
-#include "Variables.h"
+#include "ADC.h"
+#include <stdint.h>
+#include <SPI.h>
+
+#define DRDY_PIN 14
+
+#define CMD_RESET   0x0011
+#define CMD_RREG    0x2000
+#define CMD_WREG    0x4000
+#define CMD_NULL    0x0000
+
+SPIClass spi;
+
+ADC::ADC(): m_adcValues{0,0,0}{
+    pinMode(DRDY_PIN, INPUT);
+
+    // initialise spi transfer
+    //setup drdy pin pour lire lorsque c'est prêt
+}
+
+uint32_t ADC::spiTransfer24(uint32_t p_data){
+
+    uint32_t rx = 0;
+    rx |= spi.transfer((p_data >> 16) & 0xFF) << 16;
+    rx |= spi.transfer((p_data >> 8) & 0xFF) << 8;
+    rx |= spi.transfer(p_data & 0xFF);
+
+    return rx;
+}
+
+
+void ADC::writeRegister(uint8_t p_reg, uint16_t p_value)
+{
+    uint16_t cmd = CMD_WREG | (p_reg << 7) | 0;
+
+
+    spi.transfer((cmd >> 8) & 0xFF);
+    spi.transfer(cmd & 0xFF);
+
+    spi.transfer((p_value >> 8) & 0xFF);
+    spi.transfer(p_value & 0xFF);
+}
+
+uint16_t ADC::readRegister(uint8_t p_reg)
+{
+    uint16_t cmd = CMD_RREG | (p_reg << 7) | 0;
+
+    spi.transfer((cmd >> 8) & 0xFF);
+    spi.transfer(cmd & 0xFF);
+
+    uint16_t value = 0;
+    value |= spi.transfer(0x00) << 8;
+    value |= spi.transfer(0x00);
+
+    return value;
+}
+
+void ADC::readData(int32_t* p_channels)
+{
+    uint32_t status = 0;
+
+    for(int i=0;i<3;i++)
+        status = (status << 8) | spi.transfer(0);
+
+    for(int ch=0; ch<4; ch++)
+    {
+        int32_t value = 0;
+
+        value |= spi.transfer(0) << 16;
+        value |= spi.transfer(0) << 8;
+        value |= spi.transfer(0);
+
+        if(value & 0x800000)
+            value |= 0xFF000000;
+
+        p_channels[ch] = value;
+    }
+}
+
+
+void ADC::reset(){
+    spiTransfer24(CMD_RESET);
+    delay(10);
+}
+
+
+

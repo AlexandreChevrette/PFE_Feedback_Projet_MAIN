b8621cf54f1ad30ab28035092b779684d9132da4
AlexandreChevrette <alche67@ulaval.ca>
2026-03-06 20:03:29 -0500

    avancements moteur

--- code_esp32_premier_test/include/MotorControl.h
+++ code_esp32_premier_test/include/MotorControl.h
@@ -5,7 +5,28 @@
 #include <Arduino.h>
 
 
-#define PWM_FREQ_ADDR       0x18
+
+#define PWM_MAP1_ADDR       0x0F
+#define PWM_MAP2_ADDR       0x10
+#define PWM_MAP3_ADDR       0x11
+#define PWM_MAP4_ADDR       0x12
+#define MAPPING1            0x00 // 0000 0000
+#define MAPPING2            0x09 // 0000 1001
+#define MAPPING3            0x12 // 0001 0010
+#define MAPPING4            0x3F // 0011 1111
+
+#define HB_PWM_MODE_ADDR    0x0B
+#define PWM_MODE            0xFF 
+
+#define PWM_ENABLE_ADDR     0x0C
+#define PWM_ENABLE123       0xF8 // 1111 1000
+
+#define PWM1_DUTY           0x15 
+#define PWM2_DUTY           0x16 
+#define PWM3_DUTY           0x17 
+
+#define PWM_FREQ1_ADDR      0x13
+#define PWM_FREQ2_ADDR      0x14
 #define PWM_FREQ_80         0x00
 #define PWM_FREQ_2000       0xFF
 #define SPI_SCLK_SPEED_MOT  1000000
--- code_esp32_premier_test/src/MotorControl.cpp
+++ code_esp32_premier_test/src/MotorControl.cpp
@@ -5,6 +5,8 @@
 SPIClass spi3(HSPI); //SPI3
 
 // page 59 et 64
+//PAGE 94 pour les bon registers
+//PAGE 105
 
 MotorControl::MotorControl(){
 
@@ -13,11 +15,29 @@ MotorControl::MotorControl(){
 }
 
 void MotorControl::setPwmFreq(uint8_t p_pwmMode) const{
-    uint8_t write = 0b00;  
-    uint16_t address = PWM_FREQ_ADDR & 0x3F;
-    uint16_t command = (write << 6) | address;
     spi3.beginTransaction(SPISettings(SPI_SCLK_SPEED_MOT, MSBFIRST, SPI_MODE1)); 
-    spi3.transfer(command);
+    spi3.transfer(PWM_FREQ1_ADDR);
+    spi3.transfer(p_pwmMode);
+    spi3.transfer(PWM_FREQ2_ADDR);
     spi3.transfer(p_pwmMode);
     spi3.endTransaction();
 }
+
+void MotorControl::mapHalfBridges() const{
+    // to change (only one HB has pwm attached)
+    spi3.beginTransaction(SPISettings(SPI_SCLK_SPEED_MOT, MSBFIRST, SPI_MODE1)); 
+    spi3.transfer(PWM_MAP1_ADDR);
+    spi3.transfer(MAPPING1); // HB1 and 2 to PWM1
+    spi3.transfer(PWM_MAP2_ADDR);
+    spi3.transfer(MAPPING2); // HB3 and 4 to PWM2
+    spi3.transfer(PWM_MAP3_ADDR);
+    spi3.transfer(MAPPING3); // HB5 and 6 to PWM3
+    spi3.transfer(PWM_MAP4_ADDR);
+    spi3.transfer(MAPPING4); // HB7 and 8 to not used
+    spi3.endTransaction();
+}
+
+
+// pour le control, il faudra que je met un des deux en input en PWM et l'autre en 
+
+// uint16_t address = PWM_FREQ_ADDR & 0x3F; for reading i need this

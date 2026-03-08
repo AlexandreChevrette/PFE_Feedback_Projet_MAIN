8e240cde7cfd18c4de49cadbf879dc19aa1c7b3d
AlexandreChevrette <alche67@ulaval.ca>
2026-03-07 21:56:32 -0500

    almost finished?

--- code_esp32_premier_test/include/MotorControl.h
+++ code_esp32_premier_test/include/MotorControl.h
@@ -3,9 +3,12 @@
 
 #include <stdint.h>
 #include <Arduino.h>
+#include <array>
 
+#define DIRECTION_ADDR1     0x08
+#define DIRECTION_ADDR2     0x09
 
-
+// I map 2,4,6 to PWMs but I don't assign them PWM mode. 
 #define PWM_MAP1_ADDR       0x0F
 #define PWM_MAP2_ADDR       0x10
 #define PWM_MAP3_ADDR       0x11
@@ -13,17 +16,19 @@
 #define MAPPING1            0x00 // 0000 0000
 #define MAPPING2            0x09 // 0000 1001
 #define MAPPING3            0x12 // 0001 0010
-#define MAPPING4            0x3F // 0011 1111
+#define MAPPING4            0x1B // 0001 1011
 
 #define HB_PWM_MODE_ADDR    0x0B
-#define PWM_MODE            0xFF 
+#define PWM_MODE            0xD5 // 1101 0101
+// setting HB1, 3, 5, 7, 8 to PWM. (7 and 8 will be set to low) 
 
 #define PWM_ENABLE_ADDR     0x0C
-#define PWM_ENABLE123       0xF8 // 1111 1000
+#define PWM_ENABLE1234      0xF0 // 1111 0000
 
-#define PWM1_DUTY           0x15 
-#define PWM2_DUTY           0x16 
-#define PWM3_DUTY           0x17 
+#define PWM1_DUTY_ADDR      0x15 
+#define PWM2_DUTY_ADDR      0x16 
+#define PWM3_DUTY_ADDR      0x17
+#define PWM4_DUTY_ADDR      0x18
 
 #define PWM_FREQ1_ADDR      0x13
 #define PWM_FREQ2_ADDR      0x14
@@ -31,22 +36,32 @@
 #define PWM_FREQ_2000       0xFF
 #define SPI_SCLK_SPEED_MOT  1000000
 
-#define PWM_CHANNEL_1       0b00
-#define PWM_CHANNEL_2       0b01
-#define PWM_CHANNEL_3       0b10
-#define PWM_CHANNEL_4       0b11
+#define READ_ADDRESS        0x40 // concat to address to read
+
+#define IC_STATUS_ADDR      0x00
 
 const int FRAME_SIZE_BYTES_MOT = 16;
+const int numberOfMotors = 4;
 
 class MotorControl{
     public:
         MotorControl();
-        void setSpeed(uint8_t p_indiceMoteur, uint8_t p_speed) const;
-        void setPwmFreq(uint8_t p_pwmMode) const;
-        
+        void setPWM(size_t p_motorIndex, uint8_t p_pwmValue);
+        void setForward(size_t p_motorIndex);
+        void setReverse(size_t p_motorIndex);
+        // uint8_t getStatus();
 
     private:
+        std::array<uint8_t, numberOfMotors> m_pwmValues;
+        std::array<uint8_t, numberOfMotors> m_pwmDutyAddress;
+        std::array<uint8_t, numberOfMotors> m_directionAddress;
+        byte m_direction1Values;
+        byte m_direction2Values;
+        void setDirection(size_t p_motorIndex, uint8_t newValue);
         void mapHalfBridges() const;
+        void setMotorMode() const; 
+        void enablePwmChannels() const; 
+        void setPwmFreq(uint8_t p_pwmMode) const;
         // page 82
 };
 
--- code_esp32_premier_test/src/MotorControl.cpp
+++ code_esp32_premier_test/src/MotorControl.cpp
@@ -8,10 +8,51 @@ SPIClass spi3(HSPI); //SPI3
 //PAGE 94 pour les bon registers
 //PAGE 105
 
-MotorControl::MotorControl(){
+MotorControl::MotorControl(): m_pwmValues{0,0,0,0}, 
+                              m_pwmDutyAddress{PWM1_DUTY_ADDR, PWM2_DUTY_ADDR, PWM3_DUTY_ADDR, PWM4_DUTY_ADDR},
+                              m_directionAddress{DIRECTION_ADDR1, DIRECTION_ADDR1, DIRECTION_ADDR2, DIRECTION_ADDR2},
+                              m_direction1Values{0x00},
+                              m_direction2Values{0x00}
+{
 
     //à voir
     //spi3.begin(SPI_SCLK, SPI_MISO, SPI_MOSI);
+}
+
+// index 1,2,3,4 for motors
+void MotorControl::setPWM(size_t p_motorIndex, uint8_t p_pwmValue){
+    if ((p_motorIndex < numberOfMotors+1) && (p_motorIndex > 0))
+    {
+        spi3.beginTransaction(SPISettings(SPI_SCLK_SPEED_MOT, MSBFIRST, SPI_MODE1)); 
+        spi3.transfer(m_pwmDutyAddress[p_motorIndex-1]);
+        spi3.transfer(p_pwmValue);
+        spi3.endTransaction();
+        m_pwmValues[p_motorIndex-1] = p_pwmValue;
+    }
+}
+
+void MotorControl::setDirection(size_t p_motorIndex, uint8_t newValue)
+{
+    if ((p_motorIndex < numberOfMotors + 1) && (p_motorIndex > 0))
+    {
+        uint8_t* reg = (p_motorIndex <= 2) ? &m_direction1Values : &m_direction2Values;
+        uint8_t shift = (p_motorIndex % 2) ? 0 : 4;
+
+        *reg = (*reg & ~(0x0F << shift)) | ((newValue & 0x0F) << shift);
+
+        spi3.beginTransaction(SPISettings(SPI_SCLK_SPEED_MOT, MSBFIRST, SPI_MODE1));
+        spi3.transfer(m_directionAddress[p_motorIndex - 1]);
+        spi3.transfer(*reg);
+        spi3.endTransaction();
+    }
+}
+
+void MotorControl::setForward(size_t p_motorIndex){
+    setDirection(p_motorIndex, 0b1001);
+}
+
+void MotorControl::setReverse(size_t p_motorIndex){
+    setDirection(p_motorIndex, 0b0110)
 }
 
 void MotorControl::setPwmFreq(uint8_t p_pwmMode) const{
@@ -24,7 +65,6 @@ void MotorControl::setPwmFreq(uint8_t p_pwmMode) const{
 }
 
 void MotorControl::mapHalfBridges() const{
-    // to change (only one HB has pwm attached)
     spi3.beginTransaction(SPISettings(SPI_SCLK_SPEED_MOT, MSBFIRST, SPI_MODE1)); 
     spi3.transfer(PWM_MAP1_ADDR);
     spi3.transfer(MAPPING1); // HB1 and 2 to PWM1
@@ -33,11 +73,24 @@ void MotorControl::mapHalfBridges() const{
     spi3.transfer(PWM_MAP3_ADDR);
     spi3.transfer(MAPPING3); // HB5 and 6 to PWM3
     spi3.transfer(PWM_MAP4_ADDR);
-    spi3.transfer(MAPPING4); // HB7 and 8 to not used
+    spi3.transfer(MAPPING4); // HB7 and 8 to not used  
     spi3.endTransaction();
 }
 
 
-// pour le control, il faudra que je met un des deux en input en PWM et l'autre en 
+void MotorControl::setMotorMode() const{
+    spi3.beginTransaction(SPISettings(SPI_SCLK_SPEED_MOT, MSBFIRST, SPI_MODE1)); 
+    spi3.transfer(HB_PWM_MODE_ADDR);
+    spi3.transfer(PWM_MODE);
+    spi3.endTransaction();
+}
+
+void MotorControl::enablePwmChannels() const{
+    spi3.beginTransaction(SPISettings(SPI_SCLK_SPEED_MOT, MSBFIRST, SPI_MODE1)); 
+    spi3.transfer(PWM_MAP1_ADDR);
+    spi3.transfer(PWM_ENABLE1234);
+    spi3.endTransaction();
+} 
+
 
 // uint16_t address = PWM_FREQ_ADDR & 0x3F; for reading i need this

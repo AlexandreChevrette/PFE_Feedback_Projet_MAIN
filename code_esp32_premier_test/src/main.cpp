4466c124cc6469643377a086d5ae1799ef67793f
AlexandreChevrette <alche67@ulaval.ca>
2026-03-09 15:06:00 -0400

    woops

--- code_esp32_premier_test/src/main.cpp
+++ code_esp32_premier_test/src/main.cpp
@@ -13,9 +13,9 @@ FeedbackControl feedbackControl;
 WheatstoneBridge bridge1(18000, 10000, 82000, 3.3);
 WheatstoneBridge bridge2(18000, 10000, 82000, 3.3);
 WheatstoneBridge bridge3(18000, 10000, 82000, 3.3);
-PressureSensor sensor1(0.0462,82000);
-PressureSensor sensor2(0.0462,82000);
-PressureSensor sensor3(0.0462,82000);
+PressureSensor sensor1(0.0462,200000);
+PressureSensor sensor2(0.0462,200000);
+PressureSensor sensor3(0.0462,200000);
 
 void IRAM_ATTR drdyISR()
 {
